25a249e812bd7184bd280f9bc21f72ccd60a60e1
Martins Aloba <aloba.martins@gmail.com>
2021-01-08 17:15:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(element) {
+ kittens.push(element);
+ return kittens;
+}
+
+function destructivelyPrependKitten(element) {
+ kittens.unshift(element);
+ return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+ return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+ return kittens.shift();
+}
+
+function appendKitten(element) {
+  const newArray = [...kittens, element];
+  return newArray;
+}
+
+function prependKitten(element) {
+  const newArray = [element, ...kittens];
+  return newArray;
+}
